e396ed44c2bc2465a22e7ee78a988b3c0e16e4b6
hallnt <nickyb63@hotmail.com>
2015-07-07 23:51:24 -0400

    created folders and inital html, css and typeScript files

---
+++ Swift Fish/Scripts/game.ts
@@ -0,0 +1,68 @@
+﻿/// <reference path="typings/stats/stats.d.ts" />
+/// <reference path="typings/easeljs/easeljs.d.ts" />
+/// <reference path="typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="typings/soundjs/soundjs.d.ts" />
+/// <reference path="typings/preloadjs/preloadjs.d.ts" />
+ 
+// GAME FRAMEWORK VARIABLES
+var canvas = document.getElementById("canvas");
+var stage: createjs.Stage;
+var stats: Stats;
+
+var assets: createjs.LoadQueue;
+var manifest = [
+    { id: "fish", src: "assets/images/fish.png" },
+];
+
+// GAME VARIABLES
+
+
+// PRELOADER FUNCTION
+function preload() {
+    assets = new createjs.LoadQueue();
+    assets.installPlugin(createjs.Sound);
+    // event listener triggers when assets are completely loaded
+    assets.on("complete", init, this);
+    assets.loadManifest(manifest);
+    //Setup statistics object
+    setupStats();
+}
+
+// CALLBACK FUNCTION THAT INITIALIZES GAME OBJECTS
+function init() {
+    stage = new createjs.Stage(canvas); // reference to the stage
+    stage.enableMouseOver(20);
+    createjs.Ticker.setFPS(60); // framerate 60 fps for the game
+    // event listener triggers 60 times every second
+    createjs.Ticker.on("tick", gameLoop); 
+
+    // calling main game function
+    main();
+}
+
+// FUNCTION TO SETUP STAT COUNTING
+function setupStats() {
+    stats = new Stats();
+    stats.setMode(0); // set to fps
+
+    // align bottom-right
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '650px';
+    stats.domElement.style.top = '30px';
+
+    document.body.appendChild(stats.domElement);
+}
+
+// CALLBACK FUNCTION THAT CREATES OUR MAIN GAME LOOP - refreshed 60 fps
+function gameLoop() {
+    stats.begin(); // Begin measuring
+
+    stage.update();
+
+    stats.end(); // end measuring
+}
+
+// OUR MAIN GAME FUNCTION
+function main() {
+
+}
